3f7c5c3b61649410d493032e38a5e340ee0cccd0
christophe Kurzawa <christophe.kurzawa@student.howest.be>
2015-03-26 10:48:43 +0100

    - Added custom listViewItem - Added Images - Writing Custom Array Adapter

--- week06/oef1/Horoscoop/app/src/main/java/be/howest/nmct/HoroscoopActivity.java
+++ week06/oef1/Horoscoop/app/src/main/java/be/howest/nmct/HoroscoopActivity.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 
 import android.app.Activity;
 
+import android.provider.ContactsContract;
 import android.view.Menu;
 
 
@@ -10,13 +11,33 @@ import android.view.Menu;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 /**
  * Created by chris on 23/03/15.
  */
 public class HoroscoopActivity extends ListActivity {
+    class HoroscoopAdapter extends ArrayAdapter<Data.Horoscoop>{
+       public HoroscoopAdapter(){
+           super(HoroscoopActivity.this, R.layout.row_horoscoop, R.id.textViewnaamHoroscoop, Data.Horoscoop.values());
+       }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+//            return super.getView(position, convertView, parent);
+            View row = super.getView(position,convertView, parent);
+            Data.Horoscoop horoscoop = Data.Horoscoop.values()[position];
+
+            TextView textViewNaamHoroscoop = (TextView) row.findViewById(R.id.textViewnaamHoroscoop);
+            textViewNaamHoroscoop.setText(horoscoop.getNaamHoroscoop());
+
+            return row;
+        }
+    }
+
 }
