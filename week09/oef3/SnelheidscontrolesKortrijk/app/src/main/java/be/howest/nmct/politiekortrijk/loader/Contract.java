6499cb92c2d29d63eae3f8e34fe2ac89e4b65cd5
Christophe Kurzawa <christophe.kurzawa@student.howest.be>
2015-05-03 20:53:40 +0200

    --week 09 oef 03

---
+++ week09/oef3/SnelheidscontrolesKortrijk/app/src/main/java/be/howest/nmct/politiekortrijk/loader/Contract.java
@@ -0,0 +1,21 @@
+package be.howest.nmct.politiekortrijk.loader;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by chris on 3/05/15.
+ */
+public class Contract {
+    public interface snelheidColumns extends BaseColumns {
+        public static final String COLUMN_JAAR = "Jaar";
+        public static final String COLUMN_MAAND = "Maand";
+        public static final String COLUMN_STRAAT = "Straat";
+        public static final String COLUMN_POSTCODE = "Postcode";
+        public static final String COLUMN_GEMEENTE = "Gemeente";
+        public static final String COLUMN_AANTAL_CONTROLES = "Aantal controles";
+        public static final String COLUMN_GEPASSEERDE_VOERTUIGEN = "Gepasseerde voertuigen";
+        public static final String COLUMN_VTG_IN_OVERTREDING = "Vtg in overtreding";
+        public static final String COLUMN_X = "X";
+        public static final String COLUMN_Y = "Y";
+    }
+}
